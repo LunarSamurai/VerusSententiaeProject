c043db334f28430078af17ebb3bef3c3f215d7aa
LunarSamurai <josephcraig757@gmail.com>
2023-12-27 18:21:10 -0500

    Migration Changes Applied
    -- Joseph Craig

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace VerusSententiaeProject
     public partial class MainWindow : Window
     {
         private DispatcherTimer splashTimer = new DispatcherTimer();
-        private IAT _iat; 
+        private IAT _iat;
         private SAM _sam;
         private List<string> _videoFiles;
         private int _currentVideoIndex;
@@ -38,7 +38,6 @@ namespace VerusSententiaeProject
         {
 
             InitializeComponent();
-            LoadValenceImage(); 
             _iat = new IAT();
             _sam = new SAM();
             AttachIATEventHandlers();
@@ -59,11 +58,13 @@ namespace VerusSententiaeProject
         }
         private void MainWindow_KeyDown(object sender, KeyEventArgs e)
         {
-            _iat.IAT_KeyDown(e.Key);
-            if (e.KeyboardDevice.Modifiers == ModifierKeys.Alt && e.Key == Key.Y)
+            SAM.OnSkipVideoKeyPress += (e) =>
             {
-                SkipVideo();
-            }
+                if (e.Key == Key.Y)
+                {
+                    _sam.SkipVideo();
+                }
+            };
         }
 
         private void AttachIATEventHandlers()
@@ -167,7 +168,7 @@ namespace VerusSententiaeProject
         {
             splashTimer.Stop();
             // Hide SplashScreen and show MainMenu
-;           SplashScreen.Visibility = Visibility.Collapsed;
+            ; SplashScreen.Visibility = Visibility.Collapsed;
             Exam_Menu.Visibility = Visibility.Visible;
             StartScreen.Visibility = Visibility.Visible;
 
@@ -200,6 +201,7 @@ namespace VerusSententiaeProject
             Exam_Menu.Visibility = Visibility.Collapsed;
             StartScreen.Visibility = Visibility.Collapsed;
             SamStartScreen.Visibility = Visibility.Visible;
+            SAM.OnValenceRecordingStart += ValenceRecorder;
         }
 
         private void SAM_Button_Continue_Click(object sender, RoutedEventArgs e)
@@ -232,210 +234,25 @@ namespace VerusSententiaeProject
             SamDemoTitleTextBlock.Text = title;
         }
 
-        private void SAM_Instruction_Button_Continue_Click(object sender, RoutedEventArgs e)
-        {
-            SamInstructionScreen.Visibility = Visibility.Collapsed;
-            DemoIntroducer.Visibility = Visibility.Visible;
-            VideoPlayerGrid.Visibility = Visibility.Visible;
-            PlayIntroductionVideo();
-        }
-
-        private void PlayIntroductionVideo()
-        {
-            string baseDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string projectRootPath = Directory.GetParent(Directory.GetParent(Directory.GetParent(baseDir).FullName).FullName).FullName;
-            string videoFolderPath = System.IO.Path.Combine(projectRootPath, "SAM_Resources", "IntroductionVideo");
-
-            if (!Directory.Exists(videoFolderPath))
-            {
-                MessageBox.Show("Video directory not found.");
-                return;
-            }
-
-            var videoFormats = new[] { "*.mp4", "*.mov" };
-            _videoFiles = videoFormats.SelectMany(format => Directory.EnumerateFiles(videoFolderPath, format)).ToList();
-
-            if (_videoFiles.Count > 0 && _currentVideoIndex < _videoFiles.Count)
-            {
-                string videoPath = _videoFiles[_currentVideoIndex];
-                VideoPlayer.Source = new Uri(videoPath, UriKind.Absolute);
-                VideoPlayer.Play();
-            }
-            else
-            {
-                VideoPlayerGrid.Visibility = Visibility.Collapsed; // Collapse the video player if no videos are found
-            }
-        }
-
-        private void SkipVideo()
-        {
-            // Stop the current video
-            VideoPlayer.Stop();
-
-            // Increment the video index or collapse the video player grid if all videos have been played
-            _currentVideoIndex++;
-            if (_currentVideoIndex < _videoFiles.Count)
-            {
-                PlayIntroductionVideo();
-            }
-            else
-            {
-                VideoPlayerGrid.Visibility = Visibility.Collapsed;
-                MessageBox.Show("All videos have been played.");
-            }
-        }
-
-
-        private void VideoPlayer_MediaEnded(object sender, RoutedEventArgs e)
-        {
-            _currentVideoIndex++;
-            if (_currentVideoIndex < _videoFiles.Count)
-            {
-                PlayIntroductionVideo(); // Play the next video
-            }
-            else
-            {
-                VideoPlayerGrid.Visibility = Visibility.Collapsed; // Collapse the video player when all videos have been played
-                MessageBox.Show("All videos have been played.");
-            }
-        }
-
-        private void LoadAudioFiles()
-        {
-            string baseDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string projectRootPath = Directory.GetParent(Directory.GetParent(Directory.GetParent(baseDir).FullName).FullName).FullName;
-            string audioFolderPath = System.IO.Path.Combine(projectRootPath, "SAM_Resources", "Audio_Files");
-
-            if (!Directory.Exists(audioFolderPath))
-            {
-                MessageBox.Show("Audio directory not found.");
-                return;
-            }
-
-            var audioFormats = new[] { "*.mp3", "*.wav" }; // Add more formats if needed
-            var allAudioFiles = audioFormats.SelectMany(format => Directory.EnumerateFiles(audioFolderPath, format)).ToList();
-
-            foreach (var file in allAudioFiles)
-            {
-                if (System.IO.Path.GetFileName(file).Contains("trial"))
-                {
-                    trialAudioFiles.Add(file);
-                }
-                else
-                {
-                    trueTrialAudioFiles.Add(file);
-                }
-            }
-
-            // Here you can add logic to use these lists as required
-        }
-        private void OutputTrialAudioFiles()
-        {
-            if (trialAudioFiles.Count == 0)
-            {
-                // If the list is empty, print this message
-                Debug.WriteLine("The list is empty.");
-            }
-            else
-            {
-                // If the list is not empty, loop through and print each file
-                foreach (var file in trialAudioFiles)
-                {
-                    Debug.WriteLine(file);
-                }
-            }
-            Debug.WriteLine("Is this Console writing to line working at all?");
-        }
-
-        private void Demo_Introducer_Continue_Click(object sender, RoutedEventArgs e)
-        {
-            DemoIntroducer.Visibility= Visibility.Collapsed;
-            DemoTrialBeginnerGrid.Visibility = Visibility.Visible;
-            LoadAudioFiles();
-            OutputTrialAudioFiles(); // This will output the contents of trialAudioFiles
-        }
-
-        private void Demo_Trial_Beginner_Continue_Button_Click(object sender, RoutedEventArgs e)
-        {
-            SoundDisplayedGrid.Visibility = Visibility.Visible;
-            DemoTrialBeginnerGrid.Visibility = Visibility.Collapsed;
-
-            if (trialAudioFiles.Any())
-            {
-                string firstAudioFile = trialAudioFiles.First();
-                PlayAudio(firstAudioFile);
-            }
-        }
-
-        private void PlayAudio(string audioFilePath)
-        {
-            MediaPlayer mediaPlayer = new MediaPlayer();
-            mediaPlayer.Open(new Uri(audioFilePath, UriKind.RelativeOrAbsolute));
-            mediaPlayer.MediaEnded += MediaPlayer_MediaEnded;
-            mediaPlayer.Play();
-        }
-
-        private void LoadValenceImage()
-        {
-            try
-            {
-                // Get the base directory of the application.
-                string baseDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                // Navigate up to the project root (adjust the number of 'Parent' calls as needed for your directory structure).
-                string projectRootPath = Directory.GetParent(Directory.GetParent(Directory.GetParent(baseDir).FullName).FullName).FullName;
-                // Combine the project root with the relative path to the image.
-                string imagePath = System.IO.Path.Combine(projectRootPath, "SAM_Resources", "ValenceImage", "CorrectedValence.png"); // Update with the actual image name and format.
-
-                // Load the image and set it to the Image control's Source property.
-                BitmapImage image = new BitmapImage(new Uri(imagePath, UriKind.Absolute));
-                ValenceImage.Source = image; // Make sure you have an Image control with x:Name="ValenceImage" in your XAML.
-            }
-            catch (Exception ex)
-            {
-                // If there's an error loading the image, show it in a MessageBox or handle it as needed.
-                MessageBox.Show("Error loading image: " + ex.Message);
-            }
-        }
-
         private void ValenceRecorder()
         {
-            // Set focus to the main window to capture key presses
             this.Focus();
 
-            // You may also want to provide instructions to the user, e.g., via a TextBlock
-            // InstructionsTextBlock.Text = "Press a number key to rate valence.";
+            // Could show instructions here 
+
+            SAM.StartValenceRecording();
         }
 
         protected override void OnKeyDown(KeyEventArgs e)
         {
             base.OnKeyDown(e);
 
-            // Check if ValenceRatingGrid is visible and then record the key press.
-            if (ValenceRatingGrid.Visibility == Visibility.Visible)
+            if (SAM.IsValenceRecording)
             {
-                // Assuming you want to capture number keys for valence rating (1-9)
-                if (e.Key >= Key.D1 && e.Key <= Key.D9)
-                {
-                    // Store the key pressed as the current valence answer
-                    CurrentValenceAnswer = e.Key.ToString().Substring(1); // This will get the number part of the key name (D1 -> 1)
-
-                    // Optionally, you can then hide the ValenceRatingGrid and proceed to the next part of your application.
-                    ValenceRatingGrid.Visibility = Visibility.Collapsed;
-
-                    // Continue with other tasks, like showing the next audio file or saving the answer.
-                    // You might call another method here to process the answer and continue the workflow.
-                }
+                SAM.RecordValenceRating(e);
             }
         }
 
-
-        private void MediaPlayer_MediaEnded(object sender, EventArgs e)
-
-        {
-            SoundDisplayedGrid.Visibility = Visibility.Collapsed;
-            ValenceRatingGrid.Visibility = Visibility.Visible;
-            ValenceRecorder();
-        }
     }
 
 }
--- SAM.cs
+++ SAM.cs
@@ -9,13 +9,32 @@ using System.Windows.Media.Imaging;
 using System.Windows.Media;
 using System.Windows;
 using System.Windows.Input;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Controls;
 
 namespace VerusSententiaeProject
 {
-    class SAM
+    public class SAM
     {
         public string Break_Period { get; private set; }
         public List<string> Audio_File_Order { get; private set; }
+        private List<string> _videoFiles;
+        private int _currentVideoIndex;
+        private List<string> trialAudioFiles = new List<string>();
+        private List<string> trueTrialAudioFiles = new List<string>();
+        private string CurrentValenceAnswer = string.Empty;
+        public static Grid DemoIntroducer { get; set; }
+        public static Grid VideoPlayerGrid { get; set; }
+        public static Grid SamInstructionScreen { get; set; }
+        public static Grid DemoTrialBeginnerGrid { get; set; }
+        public static Grid SoundDisplayedGrid { get; set; }
+        public static Grid ValenceRatingGrid { get; set; }
+        public static Image ValenceImage { get; set; }
+        public static Image ArousalImage { get; set; }
+        public static Grid ArousalRatingGrid { get; set; }
+        public static MediaElement VideoPlayer { get; set; }
+
         // Define states for the SAM exam
         private enum SamState
         {
@@ -32,11 +51,15 @@ namespace VerusSententiaeProject
         public event Action<string> UpdateSubtitle;
         public event Action<string> ShowRandomCode;
         public event Action<string> DisplayMessage; // Event to display the message
+        public static Action OnValenceRecordingStart;
+        public static event Action<KeyEventArgs> OnSkipVideoKeyPress;
+
 
         public SAM()
         {
             Audio_File_Order = new List<string>();
             _currentState = SamState.Start;
+            LoadValenceImage();
         }
 
         public void PrepareSamExam()
@@ -67,7 +90,7 @@ namespace VerusSententiaeProject
             DisplayMessage?.Invoke(message);
         }
 
-        
+
 
         private string ReadMessageFromFile(string relativePath)
         {
@@ -104,7 +127,7 @@ namespace VerusSententiaeProject
             string fullPath = Path.Combine(projectRootPath, relativePath);
 
             // Check if the file exists
-            if (!File.Exists(fullPath)) 
+            if (!File.Exists(fullPath))
             {
                 throw new FileNotFoundException($"File not found: {fullPath}");
             }
@@ -141,6 +164,198 @@ namespace VerusSententiaeProject
                 }
             }
 
+
+        }
+        private void SAM_Instruction_Button_Continue_Click(object sender, RoutedEventArgs e)
+        {
+            SamInstructionScreen.Visibility = Visibility.Collapsed;
+            DemoIntroducer.Visibility = Visibility.Visible;
+            VideoPlayerGrid.Visibility = Visibility.Visible;
+            PlayIntroductionVideo();
+        }
+
+        private void PlayIntroductionVideo()
+        {
+            string baseDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string projectRootPath = Directory.GetParent(Directory.GetParent(Directory.GetParent(baseDir).FullName).FullName).FullName;
+            string videoFolderPath = System.IO.Path.Combine(projectRootPath, "SAM_Resources", "IntroductionVideo");
+
+            if (!Directory.Exists(videoFolderPath))
+            {
+                MessageBox.Show("Video directory not found.");
+                return;
+            }
+
+            var videoFormats = new[] { "*.mp4", "*.mov" };
+            _videoFiles = videoFormats.SelectMany(format => Directory.EnumerateFiles(videoFolderPath, format)).ToList();
+
+            if (_videoFiles.Count > 0 && _currentVideoIndex < _videoFiles.Count)
+            {
+                string videoPath = _videoFiles[_currentVideoIndex];
+                VideoPlayer.Source = new Uri(videoPath, UriKind.Absolute);
+                VideoPlayer.Play();
+            }
+            else
+            {
+                VideoPlayerGrid.Visibility = Visibility.Collapsed; // Collapse the video player if no videos are found
+            }
+        }
+
+        public void SkipVideo()
+        {
+            // Stop the current video
+            VideoPlayer.Stop();
+
+            // Increment the video index or collapse the video player grid if all videos have been played
+            _currentVideoIndex++;
+            if (_currentVideoIndex < _videoFiles.Count)
+            {
+                PlayIntroductionVideo();
+            }
+            else
+            {
+                VideoPlayerGrid.Visibility = Visibility.Collapsed;
+                MessageBox.Show("All videos have been played.");
+            }
+        }
+
+
+        private void VideoPlayer_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            _currentVideoIndex++;
+            if (_currentVideoIndex < _videoFiles.Count)
+            {
+                PlayIntroductionVideo(); // Play the next video
+            }
+            else
+            {
+                VideoPlayerGrid.Visibility = Visibility.Collapsed; // Collapse the video player when all videos have been played
+                MessageBox.Show("All videos have been played.");
+            }
+        }
+
+        private void LoadAudioFiles()
+        {
+            string baseDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string projectRootPath = Directory.GetParent(Directory.GetParent(Directory.GetParent(baseDir).FullName).FullName).FullName;
+            string audioFolderPath = System.IO.Path.Combine(projectRootPath, "SAM_Resources", "Audio_Files");
+
+            if (!Directory.Exists(audioFolderPath))
+            {
+                MessageBox.Show("Audio directory not found.");
+                return;
+            }
+
+            var audioFormats = new[] { "*.mp3", "*.wav" }; // Add more formats if needed
+            var allAudioFiles = audioFormats.SelectMany(format => Directory.EnumerateFiles(audioFolderPath, format)).ToList();
+
+            foreach (var file in allAudioFiles)
+            {
+                if (System.IO.Path.GetFileName(file).Contains("trial"))
+                {
+                    trialAudioFiles.Add(file);
+                }
+                else
+                {
+                    trueTrialAudioFiles.Add(file);
+                }
+            }
+
+            // Here you can add logic to use these lists as required
+        }
+        private void OutputTrialAudioFiles()
+        {
+            if (trialAudioFiles.Count == 0)
+            {
+                // If the list is empty, print this message
+                Debug.WriteLine("The list is empty.");
+            }
+            else
+            {
+                // If the list is not empty, loop through and print each file
+                foreach (var file in trialAudioFiles)
+                {
+                    Debug.WriteLine(file);
+                }
+            }
+            Debug.WriteLine("Is this Console writing to line working at all?");
+        }
+
+        private void Demo_Introducer_Continue_Click(object sender, RoutedEventArgs e)
+        {
+            DemoIntroducer.Visibility = Visibility.Collapsed;
+            DemoTrialBeginnerGrid.Visibility = Visibility.Visible;
+            LoadAudioFiles();
+            OutputTrialAudioFiles(); // This will output the contents of trialAudioFiles
+        }
+
+        private void Demo_Trial_Beginner_Continue_Button_Click(object sender, RoutedEventArgs e)
+        {
+            SoundDisplayedGrid.Visibility = Visibility.Visible;
+            DemoTrialBeginnerGrid.Visibility = Visibility.Collapsed;
+
+            if (trialAudioFiles.Any())
+            {
+                string firstAudioFile = trialAudioFiles.First();
+                PlayAudio(firstAudioFile);
+            }
+        }
+
+        private void PlayAudio(string audioFilePath)
+        {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri(audioFilePath, UriKind.RelativeOrAbsolute));
+            mediaPlayer.MediaEnded += MediaPlayer_MediaEnded;
+            mediaPlayer.Play();
+        }
+
+        private void LoadValenceImage()
+        {
+            try
+            {
+                // Get the base directory of the application.
+                string baseDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                // Navigate up to the project root (adjust the number of 'Parent' calls as needed for your directory structure).
+                string projectRootPath = Directory.GetParent(Directory.GetParent(Directory.GetParent(baseDir).FullName).FullName).FullName;
+                // Combine the project root with the relative path to the image.
+                string imagePath = System.IO.Path.Combine(projectRootPath, "SAM_Resources", "ValenceImage", "CorrectedValence.png"); // Update with the actual image name and format.
+
+                // Load the image and set it to the Image control's Source property.
+                BitmapImage image = new BitmapImage(new Uri(imagePath, UriKind.Absolute));
+                ValenceImage.Source = image; // Make sure you have an Image control with x:Name="ValenceImage" in your XAML.
+            }
+            catch (Exception ex)
+            {
+                // If there's an error loading the image, show it in a MessageBox or handle it as needed.
+                MessageBox.Show("Error loading image: " + ex.Message);
+            }
+        }
+
+        // In SAM class
+
+        public static bool IsValenceRecording { get; set; }
+
+        public static void StartValenceRecording()
+        {
+            IsValenceRecording = true;
+        }
+
+        public static void RecordValenceRating(KeyEventArgs e)
+        {
+            // Check key and record rating
+
+            IsValenceRecording = false;
+            ValenceRatingGrid.Visibility = Visibility.Collapsed;
+            ArousalRatingGrid.Visibility = Visibility.Visible;
+        }
+
+
+        private void MediaPlayer_MediaEnded(object sender, EventArgs e)
+
+        {
+            SoundDisplayedGrid.Visibility = Visibility.Collapsed;
+            ValenceRatingGrid.Visibility = Visibility.Visible;
+            OnValenceRecordingStart?.Invoke();
         }
 
     }
